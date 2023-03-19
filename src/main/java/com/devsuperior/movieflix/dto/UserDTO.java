966ba8cf8435cff74d8fbe0880f9a9210e826d66
Luis Almeida <lferreira646@gmail.com>
2023-03-19 19:50:58 -0300

    repositories  and UserDTO

---
+++ src/main/java/com/devsuperior/movieflix/dto/UserDTO.java
@@ -0,0 +1,39 @@
+package com.devsuperior.movieflix.dto;
+
+import com.devsuperior.movieflix.entities.User;
+
+import java.io.Serializable;
+
+public class UserDTO implements Serializable {
+
+    private Long id;
+    private String name;
+    private String email;
+
+    public UserDTO() {
+    }
+
+    public UserDTO(Long id, String name, String email) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
+
+    public UserDTO(User entity) {
+        id = entity.getId();
+        name = entity.getName();
+        email = entity.getEmail();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+}
