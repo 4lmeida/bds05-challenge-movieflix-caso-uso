9ef4d855cc3622a0d509b9dc500d3974c89394bf
Luis Almeida <lferreira646@gmail.com>
2023-03-21 18:48:06 -0300

    movie genre dto and controllers

--- src/main/java/com/devsuperior/movieflix/controllers/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/devsuperior/movieflix/controllers/exceptions/ResourceExceptionHandler.java
@@ -1,11 +1,15 @@
 package com.devsuperior.movieflix.controllers.exceptions;
 
 import com.devsuperior.movieflix.services.exceptions.ForbiddenException;
+import com.devsuperior.movieflix.services.exceptions.ResourceNotFoundException;
 import com.devsuperior.movieflix.services.exceptions.UnauthorizedException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import javax.servlet.http.HttpServletRequest;
+import java.time.Instant;
 
 @ControllerAdvice
 public class ResourceExceptionHandler {
@@ -17,8 +21,20 @@ public class ResourceExceptionHandler {
     }
 
     @ExceptionHandler(UnauthorizedException.class)
-    public ResponseEntity<OAuthCustomError> Unauthorized(UnauthorizedException e) {
+    public ResponseEntity<OAuthCustomError> unauthorized(UnauthorizedException e) {
         OAuthCustomError error = new OAuthCustomError("Unauthorized",  e.getMessage());
         return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(error);
+    }
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<StandardError>  resourceNotFound(ResourceNotFoundException e, HttpServletRequest request)  {
+        HttpStatus status = HttpStatus.NOT_FOUND;
+        StandardError error= new StandardError();
+        error.setTimestamp(Instant.now());
+        error.setStatus(status.value());
+        error.setError("Resource Not Found");
+        error.setMessage(e.getMessage());
+        error.setPath(request.getRequestURI());
+        return ResponseEntity.status(status).body(error);
     }
 }
--- src/main/java/com/devsuperior/movieflix/dto/GenreDTO.java
+++ src/main/java/com/devsuperior/movieflix/dto/GenreDTO.java
@@ -1,13 +1,18 @@
 package com.devsuperior.movieflix.dto;
 
 import com.devsuperior.movieflix.entities.Genre;
+import com.devsuperior.movieflix.entities.Movie;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 public class GenreDTO implements Serializable {
 
     private Long id;
     private String name;
+
+    private List<MovieDTO> movies = new ArrayList<>();
 
     public GenreDTO() {
     }
@@ -20,6 +25,11 @@ public class GenreDTO implements Serializable {
     public GenreDTO(Genre entity) {
         id = entity.getId();
         name = entity.getName();
+    }
+
+    public GenreDTO(Genre entity, List<Movie> movies) {
+        this(entity);
+        movies.forEach(movie -> this.movies.add(new MovieDTO(movie)));
     }
 
     public Long getId() {
