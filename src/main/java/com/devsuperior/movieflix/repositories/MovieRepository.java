b853a7ec2552b7d5e65494d50f600bb6bb894a97
Luis Almeida <lferreira646@gmail.com>
2023-03-22 18:40:40 -0300

    implemented test movie and genre IT

--- src/main/java/com/devsuperior/movieflix/repositories/MovieRepository.java
+++ src/main/java/com/devsuperior/movieflix/repositories/MovieRepository.java
@@ -1,5 +1,6 @@
 package com.devsuperior.movieflix.repositories;
 
+import com.devsuperior.movieflix.entities.Genre;
 import com.devsuperior.movieflix.entities.Movie;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -7,9 +8,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface MovieRepository extends JpaRepository<Movie, Long> {
 
-    @Query("SELECT obj FROM Movie obj ORDER BY obj.title")
-    Page<Movie> findByTitle(String title, Pageable pageable);
+    @Query("SELECT obj FROM Movie obj WHERE "
+            +"COALESCE(:genres) IS NULL OR obj.genre IN :genres "
+            +"ORDER BY obj.title")
+    Page<Movie> findByGenreAndSortTitle(List<Genre> genres, Pageable pageable);
 }
